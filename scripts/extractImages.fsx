a842a74771a44db25a0e1b165a123cf18a8c8f9c
gretmn102 <erlgreywatson@gmail.com>
2026-08-19 12:39:25 +0300

    wip(script)

--- scripts/extractImages.fsx
+++ scripts/extractImages.fsx
@@ -11,12 +11,80 @@ open Twine.Twee.FSharp.Printer
 
 open Twine.SugarCube.FSharp
 
+module Option =
+    type Builder() =
+        member __.Bind(x, f) =
+            Option.bind f x
+        member __.Return x = Some x
+        member __.ReturnFrom x = x
+
+    let builder = Builder()
+
 module Result =
     // refactor: use function from FsharpMyExtension (see https://github.com/gretmn102/FsharpMyExtension/issues/20)
     let defaultWith defThunk (result: Result<'T, 'Error>) =
         match result with
         | Ok x -> x
         | Error msg -> defThunk msg
+
+[<RequireQualifiedAccess>]
+type DataImageFormat =
+    | Webp
+    | Png
+    | Jpeg
+    | Unknown of string
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+[<RequireQualifiedAccess>]
+module DataImageFormat =
+    open FParsec
+
+    let dic =
+        [
+            "webp", DataImageFormat.Webp
+            "png" , DataImageFormat.Png
+            "jpg" , DataImageFormat.Jpeg
+            "jpeg", DataImageFormat.Jpeg
+        ]
+
+    let parser: Parser<_, unit> =
+        let pcommon =
+            dic
+            |> Seq.sortByDescending (fun (rawName, _) -> rawName) // для жадности
+            |> Seq.map (fun (rawName, name) ->
+                pstringCI rawName >>% name
+            )
+            |> List.ofSeq
+            |> choice
+        let punknown =
+            manySatisfy (isNoneOf "\n\";") |>> DataImageFormat.Unknown
+        pcommon <|> punknown
+
+type DataImage = {
+    Format: DataImageFormat
+    /// in base64 format
+    Data: string
+}
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+[<RequireQualifiedAccess>]
+module DataImage =
+    open FParsec
+
+    let pimageFormat: Parser<_, unit> =
+        skipString "image" >>. skipChar '/'
+        >>. DataImageFormat.parser
+
+    // data:image/jpeg;base64,
+    let parser: Parser<_, unit> =
+        pipe2
+            (skipString "data" >>. skipChar ':'
+             >>. pimageFormat .>> skipChar ';')
+            (pstring "base64" >>. skipChar ',' >>. todo)
+            (fun imageFormat data -> {
+                Format = imageFormat
+                Data = data
+            })
 
 do
     let input = "src/game.twee"
@@ -31,12 +99,15 @@ do
             passageBody
             |> List.mapFold (fun line ->
                 let f (htmlElement: HtmlElement) =
-                    if htmlElement.Tag <> "img" then None
-                    else
-                        let src =
+                    Option.builder {
+                        do! if htmlElement.Tag <> "img" then None else Some ()
+                        let! src =
                             htmlElement.Attributes
                             |> HtmlElementAttributes.tryFind "src"
-                        sdf
+
+                        return src
+                    }
+
                 Result.builder {
                     let! src = HtmlElement.parse line
                     if src.Tag <> "img" then
@@ -45,6 +116,7 @@ do
                     return src
                 }
             )
+
         document
         |> List.map (fun passage ->
             { passage with
