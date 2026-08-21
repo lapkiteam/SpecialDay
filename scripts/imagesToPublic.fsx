237cb560cb8a961baff65b7546fa7ac949a86984
Agent Lapki <treesortbubble@gmail.com>
2026-08-21 11:22:15 +0300

    feat: convert images to webp (#2)

---
+++ scripts/imagesToPublic.fsx
@@ -0,0 +1,12 @@
+#!dotnet fsi
+#load "lib/ImageMagick.FSharp/src/Api.fsx"
+
+"src/images"
+|> Api.convertFolder {
+    OutputDirectory = Some "public/images"
+    FitSize = None
+    Dry = false
+    InputFormats = fun ext -> ext = ".png" || ext = ".jpeg"
+    OutputFormat = "webp"
+    Quality = Some 92
+}
